348b0e201ac49b3218498472226ca218b70b51c4
tyrone <tyronejiang980312@gmail.com>
2020-02-22 01:05:22 -0800

    nav edit

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@ to_p1 = function (){
 	$("#carousel-images-2").hide()
 	$("#carousel-images-3").hide()
 	$("#carousel-images-1").show()
-	$("#carousel-images-1").css({'opacity':'0'})
-	$("#carousel-images-1").animate({'opacity':'1'}, 500)
+	$('#carousel-images-1').css({'opacity':'0'})
+	$('#carousel-images-1').animate({'opacity':'1'}, 500)
 
 	$("#carousel-arrows-right").off()
 	$("#carousel-arrows-right").click(to_p2)
-	$("#carousel-arrows-left").off()
-	$("#carousel-images-left").click(to_p3)
+	$('#carousel-arrows-left').off()
+	$('#carousel-images-left').click(to_p3)
 
 }
 
@@ -18,13 +18,13 @@ to_p2 = function (){
 	$("#carousel-images-3").hide()
 	$("#carousel-images-1").hide()
 	$("#carousel-images-2").show()
-	$("#carousel-images-2").css({'opacity':'0'})
-	$("#carousel-images-2").animate({'opacity':'1'}, 500)
+	$('#carousel-images-2').css({'opacity':'0'})
+	$('#carousel-images-2').animate({'opacity':'1'}, 500)
 
 	$("#carousel-arrows-right").off()
 	$("#carousel-arrows-right").click(to_p3)
-	$("#carousel-arrows-left").off()
-	$("#carousel-arrows-left").click(to_p2)
+	$('#carousel-arrows-left').off()
+	$('#carousel-arrows-left').click(to_p1)
 
 }
 
@@ -32,14 +32,24 @@ to_p3 = function (){
 	$("#carousel-images-1").hide()
 	$("#carousel-images-2").hide()
 	$("#carousel-images-3").show()
-	$("#carousel-images-3").css({'opacity':'0'})
-	$("#carousel-images-3").animate({'opacity':'1'}, 500)
+	$('#carousel-images-3').css({'opacity':'0'})
+	$('#carousel-images-3').animate({'opacity':'1'}, 500)
 
 	$("#carousel-arrows-right").off()
 	$("#carousel-arrows-right").click(to_p1)
-	$("#carousel-arrows-left").off()
-	$("#carousel-arrows-left").click(to_p2)
+	$('#carousel-arrows-left').off()
+	$('#carousel-arrows-left').click(to_p2)
 }
+
+setup = function(){
+	$("#carousel-arrows-right").click(to_p2)
+	$('#carousel-arrows-left').click(to_p3)
+	$("#carousel-images-1").show()
+	
+}
+$(document).ready(setup)
+
+
 
  showMenu=function(){
         if (!display){
@@ -64,4 +74,3 @@ to_p3 = function (){
           x.style.display = ""
         }
       }
-
